0d8d1cc00ce6c31fc38805bf5276d4f113da9d6e
wiljoh <wiljoh@wiljoh-VGN-FW351J>
2013-06-28 20:46:45 -0400

    chromatic xml.

--- chromatic_imap_xml_db/chromatic_imap_xml_db.h
+++ chromatic_imap_xml_db/chromatic_imap_xml_db.h
@@ -69,11 +69,115 @@
 //hand rolled
 //generic
 #include <generic.h>
-
+//utils
+#include "chromatic_utils.h"
 
 
 namespace chromatic_imap_xml_db
 {
+		//forward
+
+		//enumerations
+		enum class chromatic_imap_mgr_xml_level : unsigned long
+		{
+			imxlCritical = 0 ,
+			imxlogic ,
+			imxlInformation ,
+			imxlDebug ,
+			imxlNoError
+		};
+
+
+		//types
+		typedef chromatic_imap_mgr_xml_level cimxl;
+
+		//exceptions
+
+		//functors
+
+		//predicates
+
+		//generic
+
+		//helpers
+		class sedna_connection
+		{
+			public:
+
+				//ctors
+				explicit sedna_connection();
+				sedna_connection( const std::string& addr ,
+								  const std::string& database ,
+								  const std::string& user ,
+								  const std::string& password ,
+								  const int sa = SEDNA_AUTOCOMMIT_OFF );
+
+
+
+				//dtor
+				virtual ~sedna_connection();
+
+				//stream
+				friend std::ostream& operator<<( std::ostream& ostr, const sedna_connection& sc );
+
+
+			protected:
+
+				//attributes
+				std::string m_addr;
+				std::string m_database;
+				std::string m_user;
+				std::string m_password;
+
+			private:
+
+				//no copy
+				sedna_connection( const sedna_connection& sc );
+				//no assign
+				const sedna_connection& operator= ( const sedna_connection& sc );
+
+				//attributes
+				std::unique_ptr<struct SednaConnection> m_ptr_sedna;
+
+			protected:
+
+				//attributes				s
+				int m_se_attr;
+				bool m_b_connected;
+				int m_last_return_val;
+
+			public:
+
+				//acceptors-inspectors
+				inline std::string addr() noexcept { return m_addr; }
+				inline std::string database() noexcept { return m_database; }
+				inline std::string user() noexcept { return m_user; }
+				inline std::string password() noexcept { return m_password; }
+				inline int server_attr() noexcept{ return m_se_attr; }
+				inline bool is_connected() noexcept { return m_b_connected; }
+				inline int last_return_val() { return m_last_return_val; }
+
+				//mutators
+
+				//services
+				virtual void init();
+				virtual void fini();
+				virtual void reset();
+
+
+				//immutable
+
+		};
+		inline std::ostream& operator<<( std::ostream& ostr, const sedna_connection& sc ) { return ostr << "snaps";	} // todo
+
+
+		//services
+
+		//immutable
+
+
+
+
 
 }
 namespace cixdb = chromatic_imap_xml_db;
--- chromatic_imap_xml_db/chromatic_xml_db.cpp
+++ chromatic_imap_xml_db/chromatic_xml_db.cpp
@@ -9,12 +9,26 @@
 #include "chromatic_imap_xml_db.h"
 
 using namespace chromatic_imap_xml_db;
+namespace utils = chromatic_imap_protocol_impl::chromatic_universe_utils;
+using namespace utils;
 
 using std::cout;
-
+using std::unique_ptr;
 
 int main()
 {
-	cout << "!!!Hello World!!\n" ;
+	trace_into_first( "chromatic_imap , copyright 2012 , William K. Johnson , this is the xml db module" );
+
+	try
+	{
+		unique_ptr<sedna_connection> sc( new sedna_connection() );
+	}
+	catch( ... )
+	{
+		//
+	}
+
+	trace_into_last( "chromatic_imap...fini for the xml db module");
+
 	return 0;
 }
