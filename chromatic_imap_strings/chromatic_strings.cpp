a838c0333e431c461160681490cd398c883f2070
wiljoh <wiljoh@wiljoh-VGN-FW351J>
2013-07-21 21:17:59 -0400

    imap_strings.

--- chromatic_imap_strings/chromatic_strings.cpp
+++ chromatic_imap_strings/chromatic_strings.cpp
@@ -50,8 +50,39 @@ const chromatic_fetch_dictionary chromatic_fetch_strings::imap_fetch_dictionary 
 																					 { cifc::chromatic_imap_fetch_unknown , str_fetch_unknown } };
 
 
+
+const string chromatic_fetch_strings::str_dds_retcode_ok{ "dds_retcode_ok" };
+const string chromatic_fetch_strings::str_dds_retcode_error{ "dds_retcode_error" };
+const string chromatic_fetch_strings::str_dds_retcode_unsupported{ "dds_retcode_unsupported" };
+const string chromatic_fetch_strings::str_dds_retcode_bad_parameter{ "dds_retcode_bad_parameter" };
+const string chromatic_fetch_strings::str_dds_retcode_precondition_not_met{ "dds_retcode_precondition_not_met" };
+const string chromatic_fetch_strings::str_dds_retcode_out_of_resources{ "dds_retcode_out_of_resources" };
+const string chromatic_fetch_strings::str_dds_retcode_not_enabled{ "dds_retcode_not_enabled" };
+const string chromatic_fetch_strings::str_dds_retcode_immutable_policy{ "dds_retcode_immutable_policy" };
+const string chromatic_fetch_strings::str_dds_retcode_inconsistent_policy{ "dds_retcode_inconsistent_policy" };
+const string chromatic_fetch_strings::str_dds_retcode_already_deleted{ "dds_retcode_already_deleted" };
+const string chromatic_fetch_strings::str_dds_retcode_timeout{ "dds_retcode_timeout" };
+const string chromatic_fetch_strings::str_dds_retcode_no_data{ "dds_retcode_no_data" };
+const string chromatic_fetch_strings::str_dds_retcode_illegal_operation{ "dds_retcode_illegal_operation" };
+
+
+const status_map chromatic_fetch_strings::sm { 	{ dds_retcode::dds_retcode_ok , str_dds_retcode_ok } ,
+												{ dds_retcode::dds_retcode_error , str_dds_retcode_error } ,
+												{ dds_retcode::dds_retcode_unsupported, str_dds_retcode_unsupported } ,
+												{ dds_retcode::dds_retcode_bad_parameter , str_dds_retcode_bad_parameter } ,
+												{ dds_retcode::dds_retcode_precondition_not_met , str_dds_retcode_precondition_not_met } ,
+												{ dds_retcode::dds_retcode_out_of_resources , str_dds_retcode_out_of_resources } ,
+												{ dds_retcode::dds_retcode_not_enabled , str_dds_retcode_not_enabled } ,
+												{ dds_retcode::dds_retcode_immutable_policy , str_dds_retcode_immutable_policy } ,
+												{ dds_retcode::dds_retcode_inconsistent_policy , str_dds_retcode_inconsistent_policy } ,
+												{ dds_retcode::dds_retcode_already_deleted , str_dds_retcode_already_deleted } ,
+												{ dds_retcode::dds_retcode_timeout , str_dds_retcode_timeout } ,
+												{ dds_retcode::dds_retcode_no_data , str_dds_retcode_no_data } ,
+												{ dds_retcode::dds_retcode_illegal_operation , str_dds_retcode_illegal_operation }  };
+
+
 //------------------------------------------------------------------------------------------------------------------------------------
-std::string chromatic_fetch_strings::_R( const cifc moniker )
+std::string chromatic_fetch_strings::_R( const cifc& moniker )
 {
 	ostringstream ostr;
 
@@ -66,8 +97,12 @@ std::string chromatic_fetch_strings::_R( const cifc moniker )
 		lex() == lex_case::lc_upper ? ucase( str ) : lcase( str );
 	}
 
+	str = resource_from_moniker<cifc,chromatic_fetch_dictionary>( moniker , imap_fetch_dictionary );
+
 	return str;
 }
+
+
 
 
 
