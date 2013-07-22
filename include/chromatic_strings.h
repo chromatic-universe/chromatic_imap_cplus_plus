6481f2c2cc3c2950e89932fbd218c7e833d5f7bb
wiljoh <wiljoh@wiljoh-VGN-FW351J>
2013-07-21 23:13:26 -0400

    clean.

--- include/chromatic_strings.h
+++ include/chromatic_strings.h
@@ -35,6 +35,23 @@ namespace chromatic_imap_protocol_impl
 			chromatic_imap_fetch_unknown
 	};
 	
+	enum class dds_retcode : unsigned long
+	{
+			dds_retcode_ok = 0 ,
+			dds_retcode_error ,
+			dds_retcode_unsupported ,
+			dds_retcode_bad_parameter ,
+			dds_retcode_precondition_not_met ,
+			dds_retcode_out_of_resources ,
+			dds_retcode_not_enabled ,
+			dds_retcode_immutable_policy ,
+			dds_retcode_inconsistent_policy ,
+			dds_retcode_already_deleted ,
+			dds_retcode_timeout ,
+			dds_retcode_no_data ,
+			dds_retcode_illegal_operation
+	};
+
 	enum class lex_case : unsigned long
 	{
 		lc_upper ,
@@ -42,10 +59,25 @@ namespace chromatic_imap_protocol_impl
 	};
 	
 	//types
+	typedef std::map<dds_retcode , std::string> status_map;
 	typedef std::map<chromatic_imap_fetch_const,std::string> chromatic_fetch_dictionary;
 	typedef std::unique_ptr<chromatic_fetch_strings> cfs_ptr;	
 	typedef chromatic_imap_fetch_const cifc;
 	
+
+	//generic
+	template <typename T1 , typename T2>
+	inline std::string resource_from_moniker( T1 const& moniker , T2 const& dictionary  )
+	{
+			std::ostringstream ostr;
+			std::string resource{};
+
+			auto pos = dictionary.find( moniker );
+			if ( pos != dictionary.end() ) { ostr << pos->second; resource = ostr.str(); }
+
+			return resource;
+	}
+
 	class chromatic_fetch_strings
 	{
 		public:
@@ -85,7 +117,8 @@ namespace chromatic_imap_protocol_impl
 																			   source.begin(), ::toupper ); }
 			
 			//services
-			virtual std::string _R( const cifc moniker );
+			virtual std::string _R( const cifc& moniker );
+
 
 			//immutable
 			static const std::string str_fetch_flags;
@@ -104,7 +137,22 @@ namespace chromatic_imap_protocol_impl
 			static const std::string str_fetch_header;
 			static const std::string str_fetch_headerfields;
 			static const std::string str_fetch_unknown;
-			static const chromatic_fetch_dictionary imap_fetch_dictionary; 			
+			static const chromatic_fetch_dictionary imap_fetch_dictionary;
+
+			static const std::string str_dds_retcode_ok;
+			static const std::string str_dds_retcode_error;
+			static const std::string str_dds_retcode_unsupported;
+			static const std::string str_dds_retcode_bad_parameter;
+			static const std::string str_dds_retcode_precondition_not_met;
+			static const std::string str_dds_retcode_out_of_resources;
+			static const std::string str_dds_retcode_not_enabled;
+			static const std::string str_dds_retcode_immutable_policy;
+			static const std::string str_dds_retcode_inconsistent_policy;
+			static const std::string str_dds_retcode_already_deleted;
+			static const std::string str_dds_retcode_timeout;
+			static const std::string str_dds_retcode_no_data;
+			static const std::string str_dds_retcode_illegal_operation;
+			static const status_map sm;
 			
 	};
 	
